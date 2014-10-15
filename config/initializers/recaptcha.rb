a9ca22768c61815513ad009d80f134c41a10a5fc
unknown <leonardo.collazo@AT-LSA.activotrade.com>
2014-10-15 15:37:42 +0200

    Initial commit

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LdQyfsSAAAAAMwiRDyei7YrdpwSHAqO63q5CzgI'
+  config.private_key = '6LdQyfsSAAAAAJyRkSmKDBlLEhOjZRt-oIJcL5T8'
+  #config.proxy = 'http://myproxy.com.au:8080'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '53afc03db42caae03b1d2496caad518a5242fb522c953bc38c229c403066531c20d1f5ad8cf793823e8dbf334fdf7269565f22c6e5e1f72679bee2fdc0de529f'
